a68e8e1a2a2090081ffa6c6317b272e6010462cb
crescent <anayocrescent2@gmail.com>
2024-05-19 20:14:49 +0100

    clickcart done

--- catclicker/app.js
+++ catclicker/app.js
@@ -2,7 +2,7 @@ let elem = document.getElementById("my-elem");
 let addNum = document.querySelector(".addNum");
 
 let num = 0; // Initialize the counter to 0
-
+addNum.textContent = "No of Clicks: 0";
 elem.addEventListener("click", function () {
   num = num + 1; // Increment the counter by 1 on each click
 
