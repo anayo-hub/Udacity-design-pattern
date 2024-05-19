311c0cbdb9c54fd6fd629fcac9ff33b52eb0a9ec
crescent <anayocrescent2@gmail.com>
2024-05-19 21:03:25 +0100

    clickcart final

--- catclicker/app.js
+++ catclicker/app.js
@@ -1,15 +1,14 @@
-// let elem = document.getElementById("my-elem");
-// let addNum = document.querySelector(".addNum");
+let elem = document.getElementById("my-elem");
+let addNum = document.querySelector(".addNum");
 
-// let num = 0; // Initialize the counter to 0
-// addNum.textContent = "No of Clicks: 0";
-// elem.addEventListener("click", function () {
-//   num = num + 1; // Increment the counter by 1 on each click
+let num = 0; // Initialize the counter to 0
+addNum.textContent = "No of Clicks: 0";
+elem.addEventListener("click", function () {
+  num = num + 1; // Increment the counter by 1 on each click
 
-//   // Update the content of addNum element with the updated count
-//   addNum.textContent = "No of Clicks: " + num;
-// });
-
+  // Update the content of addNum element with the updated count
+  addNum.textContent = "No of Clicks: " + num;
+});
 
 // function addProductToCart(productId) {
 //   // Check if the product is already in the cart
