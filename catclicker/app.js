19119d18c889ded21e9b53ae3e8022b2c596c3ce
crescent <anayocrescent2@gmail.com>
2024-05-19 19:40:53 +0100

    clickcart init

---
+++ catclicker/app.js
@@ -0,0 +1,11 @@
+let elem = document.getElementById("my-elem");
+let addNum = document.querySelector(".addNum");
+
+let num = 0; // Initialize the counter to 0
+
+elem.addEventListener("click", function () {
+  num = num + 1; // Increment the counter by 1 on each click
+
+  // Update the content of addNum element with the updated count
+  addNum.textContent = "No of Clicks: " + num;
+});
